48227a21a4a3599e9ef535cdd40e2399396fa0ec
tiggy31 <tigist_alemu@fitnyc.edu>
2020-08-13 15:54:18 -0400

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -3,4 +3,16 @@ class BirdsController < ApplicationController
     @birds = Bird.all
     render json: @birds
   end
+
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    render json: bird
+    # render json: { id: bird.id, name: bird.name, species: bird.species}
+    # the result is when we visist local with id 3 we''' only see the specified info
+    # or the slice method render json: bird.slice(:id, :name, :species)
+    # render json: birds, only: [:id, :name, :species] is better than slice
+    #another way is to use except,  render json: birds, except: [:created_at, :updated_at]
+    # render json: birds, except: [:created_at, :updated_at]
+  end
 end
